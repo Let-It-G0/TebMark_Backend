261978c54fd076fec0b67dd03294c5a85792a44a
최수원 <suwon3274@gmail.com>
2023-08-26 23:09:57 +0900

    🎉 :: init

---
+++ src/main/java/com/suw0n/tabmark/TabmarkApplication.java
@@ -0,0 +1,13 @@
+package com.suw0n.tabmark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TabmarkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TabmarkApplication.class, args);
+	}
+
+}
