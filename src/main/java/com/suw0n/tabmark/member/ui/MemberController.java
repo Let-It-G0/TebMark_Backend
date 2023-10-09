4331ceb46f79c991325d437ee3120da8573e5cbe
최수원 <suwon3274@gmail.com>
2023-10-09 21:32:03 +0900

    :sparkles: :: swagger 추가

--- src/main/java/com/suw0n/tabmark/member/ui/MemberController.java
+++ src/main/java/com/suw0n/tabmark/member/ui/MemberController.java
@@ -2,11 +2,14 @@ package com.suw0n.tabmark.member.ui;
 
 import com.suw0n.tabmark.member.application.GetMemberUseCase;
 import com.suw0n.tabmark.member.domain.entity.Member;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Member API")
 @RestController
 @RequestMapping(value = "/member")
 @RequiredArgsConstructor
@@ -14,6 +17,7 @@ public class MemberController {
 
     private final GetMemberUseCase getMemberUseCase;
 
+    @Operation(description = "내 회원 정보 조회")
     @GetMapping("/my")
     public Member getMember() {
         return getMemberUseCase.get();
